3a8490d1736ae1701412375a1281ae0be6df6224
cyja <cyrill.jauner@zuehlke.com>
2020-01-17 12:58:05 +0100

    use new naming convention for unit tests

--- src/test/kotlin/ch/travbit/primes/calculator/CalculatorTest.kt
+++ src/test/kotlin/ch/travbit/primes/calculator/CalculatorTest.kt
@@ -6,20 +6,27 @@ import org.junit.jupiter.api.assertThrows
 
 class CalculatorTest {
 
+    val calculator = PrimeCalculator()
+
     @Test
-    fun testBitLength() {
+    fun probablePrimeWithBitLength_WorkingBitLength_PrimeHasBitLength() {
         val bitLen = 16
-        val calculator = PrimeCalculator()
         val probablePrime = calculator.probablePrimeWithBitlength(bitLen)
         Assertions.assertEquals(bitLen, probablePrime.bitLength())
     }
 
     @Test
-    fun testIncorrectBounds() {
+    fun probablePrimeWithBitLength_TooLowBitLength_Exception() {
         val tooLowBitLen = 1
-        val tooHighBitLen = 1_000_000_000
-        val calculator = PrimeCalculator()
         assertThrows<ArithmeticException> { calculator.probablePrimeWithBitlength(tooLowBitLen) }
+    }
+
+    @Test
+    fun probablePrimeWithBitLength_TooHighBitLength_Exception() {
+        /*
+        * There is constant that defines an upper limit for the bit length of a probable prime number in BigInteger
+         */
+        val tooHighBitLen = 1_000_000_000
         assertThrows<ArithmeticException> { calculator.probablePrimeWithBitlength(tooHighBitLen) }
     }
 }
