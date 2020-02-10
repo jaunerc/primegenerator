8472691f030d39b6e2f65b98693b8bd7ecfa64ae
cyja <cyrill.jauner@zuehlke.com>
2020-02-10 13:03:41 +0100

    prevent using different urls

--- react-frontend/src/Globals.js
+++ react-frontend/src/Globals.js
@@ -1,9 +1,14 @@
 const HOST_LOCAL = 'localhost';
-const HOST_SERVER = 'backend';
+// const HOST_SERVER = 'backend';
 const PORT = '8080';
 const RUN_API = '/api';
 
 const REST_RUN_API_LOCAL = `//${HOST_LOCAL}:${PORT}${RUN_API}`;
-const REST_RUN_API_SERVER = `//${HOST_SERVER}:${PORT}${RUN_API}`;
 
-export const baseUrl = process.env.NODE_ENV === 'development' ? REST_RUN_API_LOCAL : REST_RUN_API_SERVER;
+//
+// This is a method to use different urls based on the build mode environment variable. It is actually not used.
+//
+// const REST_RUN_API_SERVER = `//${HOST_SERVER}:${PORT}${RUN_API}`;
+// export const baseUrl = process.env.NODE_ENV === 'development' ? REST_RUN_API_LOCAL : REST_RUN_API_SERVER;
+
+export const baseUrl = REST_RUN_API_LOCAL;
