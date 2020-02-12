4c809507962bfc4792af7e3e47625a4ca514f05b
cyja <cyrill.jauner@zuehlke.com>
2020-02-12 08:58:29 +0100

    add badges to identify the different frontend apps

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -5,6 +5,11 @@ import PrimeGenerator from "./component/PrimeGenerator";
 function App() {
     return (
         <div className="container">
+            <div className="row">
+                <div className="col-md-8 offset-md-2">
+                    <h4><span className="badge badge-secondary">React Frontend</span></h4>
+                </div>
+            </div>
             <PrimeGenerator/>
         </div>
     );
